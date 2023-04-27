02a0b09c9aa268ba702a4edd2b01138bf9f05c5a
Guilherme <guilherme.manzano93@gmail.com>
2023-04-26 21:04:08 -0300

    add create

--- src/main/java/com/manzano/crudspring/controller/CursoController.java
+++ src/main/java/com/manzano/crudspring/controller/CursoController.java
@@ -3,7 +3,11 @@ package com.manzano.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,12 @@ public class CursoController {
   @GetMapping
   public List<Course> list() {
     return  courseRepository.findAll();
+  }
+
+  @PostMapping
+  public ResponseEntity<Course> create(@RequestBody Course course) {
+    var save = courseRepository.save(course);
+  
+    return ResponseEntity.status(HttpStatus.CREATED).body(save);
   }
 }
